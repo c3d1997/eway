9ebb783cad88d448f6f596126e85191ee443a926
c3d1997 <102794166+c3d1997@users.noreply.github.com>
2022-07-11 21:39:13 +0800

    完整 但剩下顏色動畫跟一些小BUG尋找解決辦法中

--- js/index.js
+++ js/index.js
@@ -1,18 +1,14 @@
-function one() {
+// store
+function storeNull() {
     var storeId = document.getElementById("store_id").value;
-
     var spanShow = document.querySelector('.show');
     var errowE = document.querySelector('.errow');
-
-
     if (storeId == null || storeId == "") {
-        console.log(1);
+
         spanShow.innerHTML = ' *'
-        errowE.innerHTML = 'required'
+        errowE.innerHTML = '<p>required</p>'
         return;
     } else {
-
-
         spanShow.innerHTML = ' '
         errowE.innerHTML = ''
         return false;
@@ -20,21 +16,35 @@ function one() {
     }
 }
 
-function two() {
-    var nameId = document.getElementById("name_id").value;
+function oneCheck() {
+    var store = document.getElementById("store_id").value;
+    var spanShow = document.querySelector('.show');
+    var errowE = document.querySelector('.errow');
+    var reg = / ^[s]+|[t]+|[o]+|[r]+|[e]+|[1/2/3]$/;
 
-    var spanShow2 = document.querySelector('.show2');
-    var errowE2 = document.querySelector('.errow2');
 
-
-    if (nameId == null || nameId == "") {
-        console.log(3);
-        spanShow2.innerHTML = ' *'
-        errowE2.innerHTML = 'required'
-        return;
+    if (reg.test(store)) {
+        spanShow.innerHTML = ' '
+        errowE.innerHTML = ''
+        return true
     } else {
 
+        spanShow.innerHTML = ' *'
+        errowE.innerHTML = '<p>wrong format</p>'
 
+        return false
+    }
+}
+// name
+function nameNull() {
+    var nameId = document.getElementById("name_id").value;
+    var spanShow2 = document.querySelector('.show2');
+    var errowE2 = document.querySelector('.errow2');
+    if (nameId == null || nameId == "") {
+        spanShow2.innerHTML = ' *'
+        errowE2.innerHTML = '<p>required</p>'
+        return;
+    } else {
         spanShow2.innerHTML = ' '
         errowE2.innerHTML = ''
         return false;
@@ -42,24 +52,139 @@ function two() {
     }
 }
 
-function three() {
-    var phoneId = document.getElementById("phone_id").value;
+function twoCheck() {
+    var nameId = document.getElementById("name_id").value;
+    var spanShow2 = document.querySelector('.show2');
+    var errowE2 = document.querySelector('.errow2');
+    var reg = /^[\u4e00-\u9fa5a-zA-Z]+$/;
 
-    var spanShow3 = document.querySelector('.show3');
-    var errowE3 = document.querySelector('.errow3');
-
-
-    if (phoneId == null || phoneId == "") {
-        console.log(3);
-        spanShow3.innerHTML = ' *'
-        errowE3.innerHTML = 'required'
-        return;
+    if (reg.test(nameId)) {
+        spanShow2.innerHTML = ' '
+        errowE2.innerHTML = ''
+        return true
     } else {
 
+        spanShow2.innerHTML = ' *'
+        errowE2.innerHTML = '<p>wrong format</p>'
 
+        return false
+    }
+}
+// phone
+function phoneNull() {
+    var phoneId = document.getElementById("phone_id").value;
+    var spanShow3 = document.querySelector('.show3');
+    var errowE3 = document.querySelector('.errow3');
+    if (phoneId == null || phoneId == "") {
+        spanShow3.innerHTML = ' *'
+        errowE3.innerHTML = '<p>required</p>'
+        return;
+    } else {
         spanShow3.innerHTML = ' '
         errowE3.innerHTML = ''
         return false;
 
     }
+}
+
+function threeCheck() {
+    var phoneId = document.getElementById("phone_id").value;
+    var spanShow3 = document.querySelector('.show3');
+    var errowE3 = document.querySelector('.errow3');
+    var reg = /^0?(9)[0-9]{8}$/;
+
+    if (reg.test(phoneId)) {
+        spanShow3.innerHTML = ' '
+        errowE3.innerHTML = ''
+        return true
+    } else {
+
+        spanShow3.innerHTML = ' *'
+        errowE3.innerHTML = '<p>wrong format</p>'
+
+        return false
+    }
+}
+//amount
+function amountNull() {
+    var amountId = document.getElementById("amount_id").value;
+    var spanShow4 = document.querySelector('.show4');
+    var errowE4 = document.querySelector('.errow4');
+    if (amountId == null || amountId == "") {
+        spanShow4.innerHTML = ' *'
+        errowE4.innerHTML = '<p>required</p>'
+
+        return;
+    } else {
+        spanShow4.innerHTML = ' '
+        errowE4.innerHTML = ''
+
+        return false;
+
+    }
+}
+
+function fourCheck() {
+    var amountId = document.getElementById("amount_id").value;
+    var spanShow4 = document.querySelector('.show4');
+    var errowE4 = document.querySelector('.errow4');
+    var reg = /^[1-9]\d*|[0]$/;
+
+    if (reg.test(amountId)) {
+        spanShow4.innerHTML = ' '
+        errowE4.innerHTML = ''
+        return true
+    } else {
+
+        spanShow4.innerHTML = ' *'
+        errowE4.innerHTML = '<p>wrong format</p>'
+
+        return false
+    }
+}
+//payment
+function paymentNull() {
+    var paymentId = document.getElementById("payment_id").value;
+    var spanShow5 = document.querySelector('.show5');
+    var errowE5 = document.querySelector('.errow5');
+    if (paymentId == null || paymentId == "placeholder text") {
+        spanShow5.innerHTML = ' *'
+        errowE5.innerHTML = '<p>required</p>'
+
+        return;
+    } else {
+        spanShow5.innerHTML = ' '
+        errowE5.innerHTML = ''
+        return false;
+
+    }
+}
+
+// btn 
+function btnClick() {
+
+    var errow = document.querySelectorAll(".errow_all p");
+    var INNN = document.getElementsByTagName('input')
+    var submit = document.querySelector(".submit_ALL")
+    var sNOTE = document.querySelector('.submit_notE');
+    var a = errow.length;
+
+    console.log(a);
+
+    if (a > 0) {
+        submit.style = "background: url(/imgs/FBTN.png) no-repeat center;";
+        sNOTE.innerHTML = '<p>This person does not exist</p>'
+    } else {
+        submit.style = "background: url(/imgs/SBTN.png) no-repeat center;";
+        sNOTE.innerHTML = ''
+    }
+
+
+}
+// scroll
+
+function scrolltoF() {
+    document.querySelector('#input_froms_scroll').scrollIntoView({
+        behavior: 'smooth'
+    });
 }
